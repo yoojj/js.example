b26e87a11c90bf54ae421aef968f48954cfe9fcf
yjj <yoojjxyz@gmail.com>
2023-03-16 18:06:58 +0900

    app 설정 수정

--- deno-oak/README.md
+++ deno-oak/README.md
@@ -1,6 +1,14 @@
 # oak 
 
+https://deno.land/x/oak
+
+
 
 ```bash 
-deno run -A app.ts
+# 프로젝트 실행 
+deno run -A app.ts 
+
+
+# 플래시 서버 
+deno run --unstable -A app.ts
 ```
--- deno-oak/app.ts
+++ deno-oak/app.ts
@@ -1,17 +1,54 @@
-import { Application, FlashServer } from 'https://deno.land/x/oak/mod.ts'
-import AppRouter from './routes/index.ts'
+import { config as appEnv } from 'https://deno.land/x/dotenv/mod.ts'
+import { Application, FlashServer, hasFlash, isHttpError } from 'https://deno.land/x/oak/mod.ts'
+import { oakCors } from 'https://deno.land/x/cors/mod.ts'
+import AppRouter from './router/index.ts'
 
 
 
-const app = new Application();
+appEnv({ export: true });
 
-//app.use(logger.logger);
+const appOptions = hasFlash() ? { serverConstructor: FlashServer } : undefined;
+const app = new Application(appOptions);
 
-app.use(AppRouter.routes())
-app.use(AppRouter.allowedMethods())
+const corsOptions = {
+};
 
-app.addEventListener('listen', ({ secure, hostname, port }) => {
-   console.log(`server start port :: ${port}`)
+app.use(oakCors(corsOptions));
+
+
+
+app.use(async (ctx, next) => {
+   try {
+      await next();
+      const rt = ctx.response.headers.get('X-Response-Time');
+      console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+
+   } catch (err) {
+      if(isHttpError(err)) {
+         console.log(err)
+      }
+   }
 });
 
-await app.listen({ port: 9000 })
+app.use(async (ctx, next) => {
+   const start = Date.now();
+   await next();
+   const ms = Date.now() - start;
+   ctx.response.headers.set('X-Response-Time', `${ms}ms`);
+});
+
+
+
+AppRouter.init(app);
+
+
+
+app.addEventListener('error', (evt) => {
+   console.log(evt.error);
+});
+
+app.addEventListener('listen', ({ secure, hostname, port }) => {
+   console.log('--- server start ---')
+});
+
+await app.listen({ port: Deno.env.get('PORT') });
